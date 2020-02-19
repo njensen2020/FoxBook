55e916ac6e3e2550fb74fe3731c4c9c988b70548
Caitlin McCrory <42706467+CaitlinMcCrory@users.noreply.github.com>
2020-02-19 16:09:22 -0500

    Add files via upload

---
+++ app/src/main/res/layout/CalendarActivity.java
@@ -0,0 +1,34 @@
+package edu.rollins.foxbook;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class CalendarActivity extends AppCompatActivity{
+    private static final String TAG = "CalendarActivity";
+
+    CalendarView nCalendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView)findViewById(R.id.myDate);
+
+        nCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView nCalendarView, int year, int month, int dayOfMonth) {
+                String date = month + "/" + dayOfMonth + "/" + year;
+                myDate.setText(date);
+                Log.d(TAG, "onSelectedDayChange: " + date);
+            }
+        });
+    }
+
+}
