c4b7ae825bde6573285a62ec247a8451cc7e4581
Caitlin McCrory <42706467+CaitlinMcCrory@users.noreply.github.com>
2020-04-06 18:55:57 -0500

    Add files via upload

--- app/src/main/java/edu/rollins/foxbook/Club.java
+++ app/src/main/java/edu/rollins/foxbook/Club.java
@@ -13,15 +13,25 @@ public class Club extends AppCompatActivity {
     ImageView image;
     ListView eventsList;
 
+    String savedExtra;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
 
-        name = (TextView)findViewById(R.id.club_name);
-        contactEmail = (TextView)findViewById(R.id.contact_email);
-        bio = (TextView)findViewById(R.id.club_bio);
-        image = (ImageView)findViewById(R.id.club_image);
-        eventsList = (ListView)findViewById(R.id.club_events_list);
+        name = (TextView) findViewById(R.id.club_name);
+        contactEmail = (TextView) findViewById(R.id.contact_email);
+        bio = (TextView) findViewById(R.id.club_bio);
+        image = (ImageView) findViewById(R.id.club_image);
+        eventsList = (ListView) findViewById(R.id.club_events_list);
+
+
+        //this conditional will receive the club and event info from the databases and fill in the boxes appropriately
+        if (getIntent().getStringExtra("club") != null) {
+            savedExtra = getIntent().getStringExtra("club");
+            //now to parse . . . but how????
+            bio.setText(savedExtra);
+        }
     }
 }
--- app/src/main/java/edu/rollins/foxbook/ClubDatabaseHelper.java
+++ app/src/main/java/edu/rollins/foxbook/ClubDatabaseHelper.java
@@ -1,5 +1,61 @@
 package edu.rollins.foxbook;
 
-public class ClubDatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ClubDatabaseHelper extends SQLiteOpenHelper {
     //creates and accesses database of clubs
+    //QUESTION! if this the club primary key is linked to event does that mean that they HAVE to be separate tables, but the same database?????
+    public static final String DATABASE_NAME = "Club.db";   //does this need to match Event.db in order for them to work together????
+    public static final String TABLE_NAME = "club_table";
+    public static final String COL_1 = "NAME";              //primary key
+    public static final String COL_2 = "EMAIL";
+    public static final String COL_3 = "BIO";
+    //public static final String COL_4 = "EVENTS";          //no need for this 'cause events will be printed via useage of club attribute in the event DB!!!
+    public static final String COL_4 = "IMAGE";             //holds url for club image??? is that even possible???
+
+    public ClubDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {                       //creates database with given table
+        db.execSQL("create table " + TABLE_NAME + " ("+ COL_1 + " TEXT, " + COL_2 + " TEXT, " + COL_3 + " TEXT, " + COL_4 + " TEXT )");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String email, String bio, String image) {        //inserts new club into database
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, name);
+        contentValues.put(COL_2, email);
+        contentValues.put(COL_3, bio);
+        contentValues.put(COL_4, image);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) {                                          //returns true or false to tell if insertion was successful or not
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    //needs this update method because clubs should be able to edit their pages in the case of adding a new contact email, updating bio, changing image etc.
+    public boolean updateData(String name, String email, String bio, String image) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        //SQLiteDatabase db = this.getReadableDatabase(); <-- should it be readable????
+
+        db.execSQL("UPDATE " + TABLE_NAME + " SET " + COL_2 + " = \'" + email + "\' WHERE " + COL_1 + " = \'" + name + "\'");
+        db.execSQL("UPDATE " + TABLE_NAME + " SET " + COL_3 + " = \'" + bio + "\' WHERE " + COL_1 + " = \'" + name + "\'");
+        db.execSQL("UPDATE " + TABLE_NAME + " SET " + COL_4 + " = \'" + image + "\' WHERE " + COL_1 + " = \'" + name + "\'");
+
+        return true;
+    }
 }
